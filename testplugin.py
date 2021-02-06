dc49b05f68e05aca5514264af2e636c5c9491aa7
Aaron <aaronwey@posteo.de>
2021-02-06 09:24:56 +0100

    Implements reseting the buffer on each change
    Took 35 minutes

--- testplugin.py
+++ testplugin.py
@@ -1,42 +1,57 @@
+from pathlib import Path
+
 import pynvim
 from pynvim import Nvim
 
 
+def load_def_buffer() -> str:
+
+    abs_file_path = Path("/home/aaron/Code/Python/VimTraining/default_buffer.txt")
+
+    with open(abs_file_path, "r") as file:
+        result = file.read()
+
+    return result
+
+    pass
+
+
 @pynvim.plugin
 class TestPlugin(object):
-
-    def __init__(self, nvim:Nvim):
+    def __init__(self, nvim: Nvim):
         self.nvim = nvim
+        self.default_buffer = load_def_buffer()
+        pass
 
-    @pynvim.function('TestFunction', sync=True)
-    def testfunction(self, args):
-        self.nvim.out_write("5")
-        return 5
-
-    @pynvim.command('TestCommand', nargs='*', range='')
-    def testcommand(self, args, range):
-        self.nvim.current.line = ('Command with args: {}, range: {}'
-                                  .format(args, range))
-
-    @pynvim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")', sync=True)
+    @pynvim.autocmd("BufEnter", pattern="*.py", eval='expand("<afile>")', sync=True)
     def on_bufenter(self, filename):
-        #result = self.nvim.api.strwidth("some text")
+        pass
 
-        #self.nvim.out_write(str(result))
-
-        print("5")
-        length = self.nvim.request("nvim_buf_line_count", self.nvim.current.buffer)
-        with open("/tmp/log.txt", 'a') as file:
-            file.write(str(length))
-        self.nvim.out_write('testplugin is in ' + filename + '\n')
-
-    @pynvim.autocmd('TextChanged', pattern='*.py', eval='expand("<afile>")', sync=True)
+    @pynvim.autocmd("TextChanged", pattern="*.py", eval='expand("<afile>")', sync=True)
     def on_change(self, filename):
-        with open("/tmp/log.txt", 'a') as file:
-            file.write("Text changed \n\n")
-        self.nvim.out_write('testplugin is in ' + filename + '\n')
 
-    @pynvim.autocmd('TextChangedI', pattern='*.py', eval='expand("<afile>")', sync=True)
+        str_list = ["halloss", "tschüss"]
+        self.nvim.api.buf_set_lines(0, 0, 3, False, str_list)
+
+        with open("/tmp/log.txt", "a") as file:
+            file.write("Text changed \n\n")
+        self.nvim.out_write("testplugin is in " + filename + "\n")
+
+    @pynvim.autocmd("TextChangedI", pattern="*.py", eval='expand("<afile>")', sync=True)
     def on_change_i(self, filename):
-        with open("/tmp/log.txt", 'a') as file:
-            file.write("Text changed with i\n")
+
+        buffer_list = [str(el) for el in self.default_buffer]
+
+        def map_white_space(el: str) -> str:
+            if el == "\n":
+                return ""
+            return el
+
+        white_spaced_list = list(map(map_white_space, buffer_list))
+        self.nvim.api.buf_set_lines(
+            0, 0, len(white_spaced_list), False, white_spaced_list
+        )
+
+
+if __name__ == "__main__":
+    test_obj = TestPlugin(None)
